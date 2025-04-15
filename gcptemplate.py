371fdfef1b6f8d4650e3457284d54d9d8253a150
Henry <sensork82@gmail.com>
2025-04-15 20:26:59 +0000

    Update gcptemplate.py

--- gcptemplate.py
+++ gcptemplate.py
@@ -10,6 +10,7 @@ def parse_args():
     parser.add_argument( "--location", type=str,  default='us-central1', help="Location" )
     parser.add_argument( "--bucket_uri", type=str,  default=None, help="Bucket URI" )
     parser.add_argument( "--script_path", type=str,  default=None, help="Script Path" )
+    parser.add_argument( "--job_name", type=str,  default='flower-sdk-job', help="Custom Job Name" )
 
     args = parser.parse_args()
 
@@ -18,43 +19,33 @@ def parse_args():
 
 
 def main(args):
-    print(args)
+    
     PROJECT_ID = args.project_id
     LOCATION = args.location
     BUCKET_URI=args.bucket_uri
     SCRIPT_PATH = args.script_path
+    JOB_NAME = args.job_name
 
     # Model Training
     aiplatform.init(project=PROJECT_ID, location=LOCATION, staging_bucket=BUCKET_URI)
     
     job = aiplatform.CustomJob.from_local_script(
-        display_name='flower-sdk-job',
+        display_name=JOB_NAME,
         script_path=SCRIPT_PATH,
         container_uri= 'us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-9.py310:latest',
         enable_autolog=False,
     )
 
-    job.run( )
+    job.run()
 
-    print(job)
-    # my_job = aiplatform.CustomContainerTrainingJob(
-    #     display_name = 'flower-sdk-job',
-    #     container_uri = 'us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-9.py310:latest',
-    #     staging_bucket = args.bucket_uri
-    # )
-
-    # my_job.run(
-    #     replica_count=1,
-    #     machine_type='n1-standard-8',
-    #     accelerator_type='NVIDIA_TESLA_V100',
-    #     accelerator_count=1
-    # )
+    
+    
 
     # Upload Model to Registry"""
 
     # my_model = aiplatform.Model.upload(display_name='flower-model',
     #                                   artifact_uri='gs://{PROJECT_ID}-bucket/model_output',
-    #                                   serving_container_image_uri='us-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-14:latest')
+    #                                   serving_container_image_uri='us-docker.pkg.dev/vertex-ai/training/sklearn-cpu.1-6:latest')
 
     # Deploy Model to Endpoint"""
 
