a5d0caf3ba67a4e4b95c353a3b1eb1b5857f3b51
Henry <sensork82@gmail.com>
2025-05-23 17:18:28 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 import os
 import requests
+import zipfile
+from io import BytesIO
 
-def download_data(data_url=None, local_file=None):
+def download_data(data_url=None, data_format='pdf'):
     os.makedirs("data", exist_ok=True)
     response = requests.get(data_url)
-    pdf_path = "data/sample.pdf"
+    
+    if response.status_code != 200:
+        raise Exception("Failed to download file, try again")
 
-    with open(pdf_path, "wb") as f:
-        f.write(response.content)
-    print("Downloaded and saved into data directory")
+    if data_format == 'pdf':
+        pdf_path = "data/sample.pdf"
+        with open(pdf_path, "wb") as f:
+            f.write(response.content)
+    elif data_format == 'zip':
+         with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
+            zip_ref.extractall(extract_to)
+        
+    print("Downloaded and saved into the data directory")
+
+ 
