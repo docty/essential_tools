a5c7d1a9d5f692bc69a51642c3617b339573896c
Henry <sensork82@gmail.com>
2025-09-22 00:25:24 +0000

    Update folderToHFDataset.py

--- folderToHFDataset.py
+++ folderToHFDataset.py
@@ -48,24 +48,23 @@ class FolderToHFDataset:
         return imagePath, labelPath, categories
 
     def getDataset(self):
+        data_container = {}
         # Train
         image_paths_train, label_paths_train, categories = self.extractData(self.trainDataset)
         train_dataset = self.createDataset(image_paths_train, label_paths_train, categories)
-
+        data_container['train'] = train_dataset
+        
         # Validation
         if self.valDataset != None:
             image_paths_validation, label_paths_validation, _ = self.extractData(self.valDataset)
             validation_dataset = self.createDataset(image_paths_validation, label_paths_validation, categories)
-
+            data_container['validation'] = validation_dataset
         # Test
         if self.testDataset != None:
             image_paths_test, label_paths_test, _ = self.extractData(self.testDataset)
             test_dataset = self.createDataset(image_paths_test, label_paths_test, categories)
-
-        dataset = DatasetDict({
-            "train": train_dataset,
-            "validation": validation_dataset if self.valDataset != None else None,
-            "test": test_dataset if self.testDataset != None else None,
-            })
+            data_container['test'] = test_dataset
+            
+        dataset = DatasetDict(data_container)
         
         return dataset
